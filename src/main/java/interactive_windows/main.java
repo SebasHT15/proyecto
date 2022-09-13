040b9d134d235043307e23b566e79a755782ea9c
Sebastian Hernandez <sebhernandez@estudiantec.cr>
2022-09-12 18:10:46 -0600

    Proyecto funcionando.
    Las pantallas funcionan.

--- src/main/java/interactive_windows/main.java
+++ src/main/java/interactive_windows/main.java
@@ -5,21 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/VentanaInicio.fxml"));
-        Parent root = loader.load();
+        Parent root = FXMLLoader.load(getClass().getResource("VentanaInicio.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        VentanaIniController controller = loader.getController();
-        controller.setStage(stage);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
-
 }
